ac7c4dcc9ce98fc82b9bcbc778c2e4b4fe4574ba
Kelly Dong <dongweiling27@gmail.com>
2020-09-14 20:59:47 -0400

    added UserDefaults to load and save data

--- tipCal/ViewController.swift
+++ tipCal/ViewController.swift
@@ -18,9 +18,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var peopleField: UITextField!
     @IBOutlet weak var perPersonLabel: UILabel!
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let bill = defaults.double(forKey: "myBill")
+        let people = defaults.integer(forKey: "myPeople")
+        let tipPercentage = defaults.integer(forKey: "myTipControl")
+        
+        billField.text = String(bill)
+        peopleField.text = String(people)
+        tipControl.selectedSegmentIndex = tipPercentage
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let perPerson = total/Double(people)
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format:"$%.2f", total)
+        perPersonLabel.text = String(format:"$%.2f", perPerson)
     }
 
     @IBAction func onTap(_ sender: Any) {
@@ -32,7 +49,7 @@ class ViewController: UIViewController {
         let bill = Double(billField.text!) ?? 0
         
         // people divison
-        let people = Double(peopleField.text!) ?? 1
+        let people = Int(peopleField.text!) ?? 1
         
         let tipPercentages = [0.15, 0.18, 0.2]
         
@@ -40,13 +57,18 @@ class ViewController: UIViewController {
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
-        let perPerson = total/people
+        let perPerson = total/Double(people)
         
         //update tip and total label
         tipLabel.text = String(format:"$%.2f", tip)
         totalLabel.text = String(format:"$%.2f", total)
         perPersonLabel.text = String(format:"$%.2f", perPerson)
         
+        defaults.set(bill, forKey: "myBill")
+        defaults.set(people, forKey: "myPeople")
+        defaults.set(tipControl.selectedSegmentIndex, forKey: "myTipControl")
+        
+        defaults.synchronize()
     }
     
 }
